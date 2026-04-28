c7baaee44ea3bb6ab699faf423a713238b086e2e
KatanoKeisuke <keisuke.katano.howsplan@gmail.com>
2026-04-28 14:53:26 +0900

    commit

---
+++ Ex_02_12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex_02_12;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("値を入力してください->");
+　　　　　int value = int.Parse(Console.ReadLine());
+        Console.WriteLine(Mul3(value));
+
+        int Mul3(int num)
+        {
+            return num * 3;
+        }
+    }
+
+
+}
---
+++ Ex_03_01/Item.cs
@@ -0,0 +1,7 @@
+namespace Ex_03_01;
+
+public class Item
+{
+
+
+}
