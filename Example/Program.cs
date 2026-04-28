f533220802b03b4ed9525cb7b4857f86eeebfddd
KatanoKeisuke <keisuke.katano.howsplan@gmail.com>
2026-04-28 13:43:20 +0900

    a

--- Example/Program.cs
+++ Example/Program.cs
@@ -2,24 +2,16 @@
 
 static class Program
 {
-            static void Main(string[] args)
-            {
-                Console.Write("値を入力してください->");
-                object obj = Console.ReadLine();
-                switch (obj)
+            static void Main(string[] args)     
+    {
+                for (int i = 0; i < 6; i++)
                 {
-                case int n:
-                Console.WriteLine($"整数型:{n}");
-                break;
-                case string s:
-                Console.WriteLine($"文字列型:{s}");
-                break;
-                default:
-                Console.WriteLine("他のデータ型");
-                break;
+                if (i == 2)
+                {
+                continue;
                 }
-            }
-
-
-   
+                Console.WriteLine($"i = {i}");
+                }
+                Console.WriteLine("終了");            
+    }
 }
